c5399fa613a28bcc8721e8a5fc5132f8791153bb
Matias Diaz <matidiaz00@gmail.com>
2023-03-27 03:43:43 -0300

    Adding SEO metrics and fix bugs in items for performance

--- components/items.js
+++ components/items.js
@@ -1,13 +1,13 @@
 import ItemComponent from '@/components/item'
 import { useState } from 'react';
 
-export default function ItemsComponent({ defaultItems, search, total, defaultOffset, limit }) {
+export default function ItemsComponent({ defaultItems, search, total, limit }) {
 
     const [items, addItems] = useState(defaultItems);
-    const [offset, changeOffset] = useState(defaultOffset);
+    const [offset, changeOffset] = useState(6);
 
     const getMore = async () => {
-        changeOffset(offset + default_items_lenght)
+        changeOffset(offset + limit)
         const response = await fetch(`/api/items?search=${search}&offset=${offset}&limit=${limit}`)
         const newItems = await response.json()
         addItems([...items, ...newItems.items])
@@ -22,14 +22,14 @@ export default function ItemsComponent({ defaultItems, search, total, defaultOff
             { !items || items.length != 0 ? ( <>
                 <div className='row align-items-stretch my-n3'>
                 { items ? items.map(item => 
-                    <div key={item.id} className="col-6 col-lg-4 py-3">
+                    <div key={item.id} className="itemComponent col-6 col-lg-4 py-3">
                         <ItemComponent data={item} />
                     </div>
                 ) : null}
                 </div>
 
                 <div className='my-4 d-flex d-flex justify-content-center'>
-                { offset >= total - defaultOffset ? (
+                { offset >= total ? (
                     <span className='text-light text-center'>No hay más resultados</span>
                 ) : (
                     <button type='button' className='btn btn-link' onClick={ getMore }>Cargar más</button>
